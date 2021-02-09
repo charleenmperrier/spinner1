b14c0528897b7b7fbbc0b4f46784c3fea415d0e0
charleenmperrier <charlee_perrier@hotmail.com>
2021-02-09 20:43:39 +0000

    annimation

---
+++ spinner1.js
@@ -0,0 +1,43 @@
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 800);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1000);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 1100);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 1200);
+
+setTimeout(() => {
+  process.stdout.write('\r|   \n');
+}, 1300);
+
+// ... fill in the rest yourself ...
